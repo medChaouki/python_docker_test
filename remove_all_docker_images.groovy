726e559e1c15bf2e71e6b898600c3b1f8e82f924
Mohamed Chaouki CHAMSI <chamsi.medchaouki@gmail.com>
2023-11-28 14:59:56 +0100

    Add files via upload

--- remove_all_docker_images.groovy
+++ remove_all_docker_images.groovy
@@ -2,19 +2,10 @@ pipeline {
     agent any
 
     stages {
-        stage('Remove all Docker containers') {
+        stage('Remove all Docker images') {
             steps {
                 script {
-                    // List all Docker containers
-                    def dockerContainers = sh(script: 'docker ps -aq', returnStdout: true).trim()
-
-                    // If there are any Docker containers, remove them
-                    if (dockerContainers) {
-                        sh 'docker rm -f $(docker ps -aq)'
-                        echo 'All Docker containers have been removed'
-                    } else {
-                        echo 'No Docker containers to remove'
-                    }
+                    sh 'sudo docker image prune -a -f'
                 }
             }
         }
